14a89bf516bb930c8caf60b5c5b24afeda0d2dd8
Sander Schekman <sanderschekman@gmail.com>
2022-05-07 13:14:11 +0200

    Init commit

---
+++ queries.ts
@@ -0,0 +1,12 @@
+import { gql } from "@apollo/client";
+
+export const EPISODES = gql`
+  query {
+    episodes {
+      results {
+        name,
+        air_date
+      }
+    }
+  }
+`;
---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client';
+import { Container, Grid, GridItem } from '@chakra-ui/react'
+import logo from './logo.svg'
+import './App.css'
+
+const client = new ApolloClient({
+  uri: 'https://rickandmortyapi.com/graphql',
+  cache: new InMemoryCache()
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Grid templateColumns='repeat(5, 1fr)' gap={6}>
+        <GridItem w='100%' h='10' bg='blue.500' />
+        <GridItem w='100%' h='10' bg='blue.500' />
+        <GridItem w='100%' h='10' bg='blue.500' />
+        <GridItem w='100%' h='10' bg='blue.500' />
+        <GridItem w='100%' h='10' bg='blue.500' />
+      </Grid>
+      {/* <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hello Vite + React!</p>
+          <p>
+            <button type="button" onClick={() => setCount((count) => count + 1)}>
+              count is: {count}
+            </button>
+          </p>
+          <p>
+            Edit <code>App.tsx</code> and save to test HMR updates.
+          </p>
+          <p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            {' | '}
+            <a
+              className="App-link"
+              href="https://vitejs.dev/guide/features.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Vite Docs
+            </a>
+          </p>
+        </header>
+      </div> */}
+    </ApolloProvider>
+  )
+}
+
+export default App
